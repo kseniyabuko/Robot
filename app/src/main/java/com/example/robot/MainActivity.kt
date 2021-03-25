2a352c5d403ef401338a9af981c21de4c5ec2c9a
kseni <kseniya.buko@gmail.com>
2021-03-25 12:47:53 +0100

    Homework Robot in Kotlin

--- app/src/main/java/com/example/robot/MainActivity.kt
+++ app/src/main/java/com/example/robot/MainActivity.kt
@@ -19,20 +19,34 @@ class MainActivity : AppCompatActivity() {
 
 
         var samsungHand1 = SamsungHand(140)
-        var sonyHand1 = SonyHand (110)
-        var toshibaHand1 = ToshibaHand (120)
+        var sonyHand1 = SonyHand(110)
+        var toshibaHand1 = ToshibaHand(120)
 
-        var samsungHead1 = SamsungHead (220)
-        var sonyHead1 = SonyHead (250)
-        var toshibaHead1 = ToshibaHead (280)
+        var samsungHead1 = SamsungHead(220)
+        var sonyHead1 = SonyHead(250)
+        var toshibaHead1 = ToshibaHead(280)
 
-        var samsungLeg1 = SamsungLeg (320)
-        var sonyLeg1 = SonyLeg (330)
-        var toshibaLeg1 = ToshibaLeg (360)
+        var samsungLeg1 = SamsungLeg(320)
+        var sonyLeg1 = SonyLeg(330)
+        var toshibaLeg1 = ToshibaLeg(360)
 
-        var robot1 = Robot (samsungHand1, toshibaHead1, sonyLeg1)
+        var robot1 = Robot(samsungHand1, toshibaHead1, sonyLeg1)
         robot1.action()
-        println (robot1.getPrice())
+        robot1.getPrice()
 
+        var robot2 = Robot(toshibaHand1, samsungHead1, samsungLeg1)
+        robot2.action()
+        robot2.getPrice()
+
+        var robot3 = Robot(sonyHand1, sonyHead1, toshibaLeg1)
+        robot3.action()
+        robot3.getPrice()
+
+        if (robot1.getPrice() > robot2.getPrice() && robot1.getPrice() > robot3.getPrice()) {
+            println("Price of Robot1 is max")
+        } else if (robot2.getPrice() > robot1.getPrice() && robot2.getPrice() > robot3.getPrice()) {
+            println("Price of Robot2 is max")
+        } else
+            println("Price of Robot3 is max")
     }
-}
+    }
